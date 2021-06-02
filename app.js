64bfe221f00777933fb31788c22c802c8e7df302
Mihir Solanki <contact@mihirsolanki.in>
2021-06-02 11:37:08 +0530

    Updated code to allow User Input for Dates and District Codes

--- app.js
+++ app.js
@@ -1,24 +1,20 @@
 //Defining constants for Mumbai and Thane
 const MUMBAI_DISTRICT_ID = 395;
 const THANE_DISTRICT_ID = 392;
-const DATE_OF_AVAILABILITY = '02-06-2021';
-
-//Defining boolean variables for avaialbility:
-let isAvailableMumbai = false;
-let isAvailableThane = false;
 
 
-//Defining Arrays for getting data out from JSON object
-let centerName, address, pincode, min_age_limit, available_capacity_dose1, vaccine = [];
+//gettting HTML Table from index.html
+let table = document.getElementById('mainTable');
 
-/*
-    This function has limit of 100 hits per 5 min interval.
-    This amounts it an average of 1 API call per 3 seconds.
-*/
+//default value is current day + 1 day i.e. next day
+let dateOfAvailability = `${new Date().getDate() + 1}-${new Date().getUTCMonth() + 1}-${new Date().getFullYear()}`;
+//default value is 392(THANE) 395(MUMBAI)
+let inputDistrictCodes = [392, 395];
 
-
+//This function has limit of 100 hits per 5 min interval.This amounts it an average of 1 API call per 3 seconds.
 async function getVaccineAvailablity(districtID, dateForCheckingAvailability) {
     console.log(`Checking for ${districtID} as on  ${dateForCheckingAvailability}`);
+
     const API_URL_GET_BY_DISCTRICT_ID = `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=${districtID}&date=${dateForCheckingAvailability}`;
     const response = await fetch(API_URL_GET_BY_DISCTRICT_ID);
     let districtName = "";
@@ -32,8 +28,6 @@ async function getVaccineAvailablity(districtID, dateForCheckingAvailability) {
     //converting API Response into JSON
     let data = await response.json();
 
-    //gettting HTML Table from index.html
-    let table = document.getElementById('mainTable');
 
     console.log(data);
     for (i = 0; i < data.sessions.length; i++) {
@@ -52,14 +46,44 @@ async function getVaccineAvailablity(districtID, dateForCheckingAvailability) {
             newCell.innerHTML = data.sessions[i].pincode;
             newCell = newRow.insertCell();
             newCell.innerHTML = data.sessions[i].available_capacity_dose1;
-
-
-            // listNames = `${listNames}</br>${data.sessions[i].name}      ${data.sessions[i].pincode}`
         }
     }
 
 }
+function search() {
+    //populating updated values from user input form into dateOfAvailability and inputDistrictCodes;
+    dateOfAvailability = document.getElementById('dateForCheckingAvailability').value;
+    inputDistrictCodes = document.getElementById('disctrictCodes').value;
+    console.log(dateOfAvailability);
+    console.log(inputDistrictCodes);
+    inputDistrictCodes = inputDistrictCodes.split(",");
 
+    //clear existing records
+    if (table.rows.length > 1) {
+        console.log(table.rows.length);
+        for (i = 0; i <= table.rows.length; i++) {
+            table.deleteRow(1);
+        }
+    }
 
-getVaccineAvailablity(MUMBAI_DISTRICT_ID, DATE_OF_AVAILABILITY);
-getVaccineAvailablity(THANE_DISTRICT_ID, DATE_OF_AVAILABILITY);
+    for (j = 0; j < inputDistrictCodes.length; j++) {
+        getVaccineAvailablity(inputDistrictCodes[j], dateOfAvailability);
+    }
+}
+
+//setting up default values
+document.getElementById('dateForCheckingAvailability').value = dateOfAvailability;
+document.getElementById('disctrictCodes').value = inputDistrictCodes;
+
+document.onkeydown = function (event) {
+    if (event.key !== undefined) {
+        code = event.key;
+    } else if (event.keyIdentifier !== undefined) {
+        code = event.keyIdentifier;
+    } else if (event.keyCode !== undefined) {
+        code = event.keyCode;
+    }
+    if (event.keyCode == 13) {
+        search();
+    }
+}
